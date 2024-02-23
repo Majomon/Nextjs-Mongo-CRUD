a1915bd322805d927eebdb7e7e999c0e887a4e5b
Mauri <mauri.monzon91@gmail.com>
2024-02-23 00:15:29 -0300

    Buscar tarea por ID

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
+import Task from "@/models/Task";
 
-export function GET(request, { params }) {
-  
-  return NextResponse.json({ message: `Obteniendo tarea ${params.id}` });
+export async function GET(request, { params }) {
+  try {
+    connectDB();
+    const taskFound = await Task.findById(params.id);
+
+    if (!taskFound) {
+      return NextResponse.json(
+        { message: "Tarea no encontrada" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(taskFound);
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
 
 export function DELETE(request, { params }) {
